03b432b3c0a974edd319cf4ad98362f9c8754307
developerjannatmim <jannatlamitlogiko@gmail.com>
2023-08-23 01:02:23 +0600

    - service container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use App\Facades\Invoice;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app()->bind('Invoice', function($app) {
+            return new Invoice();
+        });
     }
 
     /**
--- config/app.php
+++ config/app.php
@@ -184,6 +184,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Invoice' => App\Facades\InvoiceFacade::class,
     ])->toArray(),
 
 ];
